c4a5dce63984af1a641ab8fb6dbc2f6927083d07
Dick Childress <richard.childress@stratusgrid.com>
2021-11-17 11:03:39 -0500

    Added variable allowing module caller to specify retention period of DataDog Forwarder logs.

--- logs_monitoring.tf
+++ logs_monitoring.tf
@@ -8,6 +8,7 @@ resource "aws_cloudformation_stack" "datadog-forwarder" {
     DdSite            = var.dd_forwarder_dd_site
     ExcludeAtMatch    = var.log_exclude_at_match
     FunctionName      = "${var.aws_region}-${local.stack_prefix}datadog-forwarder"
+    LogRetentionInDays  = var.dd_forwarder_log_retention_in_days
     ReservedConcurrency = var.reserved_concurrency
   }
   template_url = "https://datadog-cloudformation-template.s3.amazonaws.com/aws/forwarder/${var.dd_forwarder_template_version}.yaml"
